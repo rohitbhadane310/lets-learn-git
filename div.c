1487dd6320cdfec60c7292b9077fece17174a8eb
Rohit Bhadane <rohitbhadane310@gmail.com>
2021-09-18 13:17:00 +0530

    adding implitention for div.c

---
+++ div.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a , b , sum;
+
+    printf("Enter the num: ");
+    scanf("%d%d",&a,&b);
+
+    sum = a / b;
+    printf("sum = %d",sum);
+    return 0;
+}
