9f77e0a820c6b2749ad54449a74dcb32d24a7eaa
Rohit Bhadane <rohitbhadane310@gmail.com>
2021-09-18 10:56:51 +0530

    adding sum.c[D[D[D[D[D[D[D[D[D[D[D[D[[[C[C[C[C[C[C[C[C[C[C[C[C[C[C

---
+++ sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int a , b ,sum;
+
+    printf("Enter the two numbers: ");
+    scanf("%d%d",&a,&b);
+
+    printf("sum of numbers are = %d",sum);
+    return 0;
+}
